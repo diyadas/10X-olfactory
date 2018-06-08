305665a76e8fe0243955298cb6de500c5b1e2d69
Diya Das <diyadas@users.noreply.github.com>
2018-06-08 16:35:48 -0700

    improve EDA and QC plots

--- scripts/tenx_1_filtering.R
+++ scripts/tenx_1_filtering.R
@@ -14,10 +14,10 @@ option_list <- list(
 )
 opt <- parse_args(OptionParser(option_list=option_list))
 exptstr <- opt$expt
-outdir <- file.path("../output", expt, "data")
-vizdir <- file.path("../output", expt, "viz")
-crdir <- file.path("../output", expt, "cr")
-exptinfo <- read.csv(file.path("../output", expt, opt$exptinfo), stringsAsFactors = FALSE)
+outdir <- file.path("../output", exptstr, "data")
+vizdir <- file.path("../output", exptstr, "viz")
+crdir <- file.path("../output", exptstr, "cr")
+exptinfo <- read.csv(file.path("../output", exptstr, opt$exptinfo), stringsAsFactors = FALSE)
 
 # Set up packages and parallel environment
 library(BiocParallel)
@@ -36,18 +36,8 @@ library(zinbwave)
 
 library(scales)
 
-# Custom code
-# Create color palettes
-pal <- clusterExperiment::bigPalette
-colb <- c("dodgerblue3", "darkviolet", "goldenrod", "chartreuse3", "cadetblue2", "magenta", "chocolate1", "forestgreen", "plum", "azure3", "chocolate4", "darkslategray", "cornsilk", "aquamarine3", "burlywood3", "darkblue", "gray45")
-cole <- c("cornflowerblue", "darkgoldenrod", "darkorchid", "darkorange2", "deeppink3", "cadetblue1", "azure4", "darkslateblue", "darkolivegreen1", "antiquewhite2")
-fastpca <- function(expr, scale=FALSE) {
-  k <- 50
-  svd_raw <- rARPACK::svds(scale(t(expr), center=TRUE, scale=scale), k=k, nu=k, nv=0)
-  pc_raw <- svd_raw$u %*% diag(svd_raw$d[1:k])
-  return(pc_raw)
-}
-
+# Source helper functions
+source("tenx_helper.R")
 
 # ---- Loading data ----
 # Load counts, batch, expt and create SummarizedExperiment
@@ -61,121 +51,74 @@ batch <- as.factor(as.vector(apply(exptinfo, 1, function(crexpt){
   return(rep(crexpt[3], ncol(exprs(crmat))))
 })))
   
-counts <- as.matrix(exprs(load_cellranger_matrix(file.path(crdir,opt$aggr))))
+counts <- as.matrix(exprs(load_cellranger_matrix(file.path(crdir, opt$aggr))))
 
 se <- SummarizedExperiment(list(counts=counts), colData=data.frame(batch=batch, expt=expt))
-genes <- read.table(file=file.path(crdir, opt$aggr, "/outs/filtered_gene_bc_matrices_mex",opt$annotation,"genes.tsv"))
+genes <- read.table(file=file.path(crdir, opt$aggr, "/outs/filtered_gene_bc_matrices_mex", opt$annotation, "genes.tsv"))
 colnames(genes) <- c("geneID", "Symbol")
 rowData(se) <- genes
-head(rowData(se))
-head(colData(se))
-
+rownames(se) <- rowData(se)$Symbol
 
 # Exploratory Data Analysis
+pdf(file=file.path(vizdir, paste0(exptstr, "_EDA_prefilt.pdf")), length=11, width=8.5)
+makeEDAplots(se, "Pre-filtering\n") # function in helper script
+dev.off()
 
-hist(colSums(assay(se)), breaks=30, 
-     xlab='Number of UMI', main="Number of UMI per sample")
 
-hist(colSums(assay(se)>0)/nrow(se), breaks=30, 
-     xlab="Proportion of detected genes", main="Proportion of detected genes")
-
-hist(colSums(assay(se)>0), breaks=30, 
-     xlab="Number of detected genes", main="Number of detected genes")
-
-hist(colMeans(assay(se)==0), breaks=30, 
-     xlab="Proportion of zeros", main="Proportion of zeros")
-
-boxplot(colSums(assay(se))~colData(se)$batch, main="Number of UMI per sample", col=colb, las=2)
-boxplot(colSums(assay(se)>0)~colData(se)$batch, main="Number of detected genes", col=colb, las=2)
-boxplot(colMeans(assay(se)==0)~colData(se)$batch, main="Proportion of zeros", col=colb, las=2)
-
-simple <- assay(se)[rowSums(assay(se))>10,]
-simple <- SUM_FN(simple)
-
-runQCmetrics <- FALSE
+# Calculate QC metrics
+runQCmetrics <- FALSE; fast <- FALSE
 if (runQCmetrics) {
-  pca <- prcomp(t(simple), scale. = TRUE)
-  # pca <- fastpca(log2(simple + 0.1))
-  ###---Use scater package to calculate some quality control metrics. Assess QC metrics.
+  simple <- assay(se)[rowSums(assay(se))>10,]
+  simple <- SUM_FN(simple)
+  pca <- ifelse(fast, fastpca(log2(simple + 0.1)), prcomp(t(simple), scale. = TRUE))
   
-  ###only works with unique identifiers - Ensembl ids, not gene names b/c some gene name duplication (65/28000)
-  sce <- newSCESet(countData = assay(se))
+  # Use scater package to calculate some quality control metrics.
+  # only works with unique identifiers - Ensembl ids, not gene names b/c some gene name duplication (65/28000)
+  sce <- SingleCellExperiment(countData = assay(se))
   sce <- calculateQCMetrics(sce)
+  sce <- as(se, "SingleCellExperiment")
   
-  rownames(se) <- rowData(se)$Symbol
-  head(rownames(se))
-  
-  ###only works with gene names
+  # Calculate mitochondrial and ribosomal percentage in each sample
   ribo_idx <- grep("^Rpl|^Rps", rowData(se)[,2])
   mito_idx <- grep("^Mt", rowData(se)[,2])
   ribo_pct <- colSums(assay(se)[ribo_idx,])/colSums(assay(se)) * 100
   mito_pct <- colSums(assay(se)[mito_idx,])/colSums(assay(se)) * 100
-  plot(mito_pct, col=colb[batch], xlab="cell index", main="% mito (Mt*) genes"); legend("topleft", legend=levels(batch), fill=colb, cex=0.8)
-  boxplot(mito_pct ~ colData(se)$batch, main="percent mito genes", col=colb, las=2, cex.axis=0.7)
-  plot(ribo_pct, col=colb[batch], xlab="cell index", main="% ribo (Rpl*) genes"); legend("topleft", legend=levels(batch), fill=colb, cex=0.8)
-  boxplot(ribo_pct ~ colData(se)$batch, main="percent ribo genes", col=colb, las=2, cex.axis=0.7)
   
   qc <- as.matrix(data.frame(colData(sce)[,c(2, 4:8)], mito_pct = mito_pct, ribo_pct = ribo_pct))
-  
   qcpca <- prcomp(qc, scale. = TRUE)
-  screeplot(qcpca, type = "lines", main = "QC-PCA screeplot")
-  plot(qcpca$x, col=alpha(colb[batch], 0.3), pch=19, main="QC PCA")
-  legend("topleft", legend=levels(batch), fill=colb, cex=0.5)
-  
-  rm(mito_idx, ribo_idx, fig_pca, fig_qpca)
-  save(se, sce, qc, pca, qcpca, file=paste0("dataObjects/", exptStr, "_unfiltered.Rda"))
+   
+  save(se, sce, qc, pca, qcpca, file=file.path(outdir, paste0(exptstr, "_prefilt.Rda")))
 } else {
-  load(paste0("dataObjects/", exptStr, "_unfiltered.Rda"))
+  load(file.path(outdir, paste0(exptstr, "_prefilt.Rda")))
 }
 
-boxplot(qc[,7] ~ colData(se)$batch, main="percent mito genes", col=colb, las=2, cex.axis=0.7)
-boxplot(qc[,8] ~ colData(se)$batch, main="percent ribo genes", col=colb, las=2, cex.axis=0.7)
+pdf(file=file.path(vizdir, paste0(exptstr, "_QCscatter_prefilt.pdf")), length=11, width=8.5)
+plot(qc$mito_pct, col=colb[batch], xlab="cell index", main="% mito (Mt*) genes"); legend("topleft", legend=levels(batch), fill=colb, cex=0.8)
+boxplot(qc$mito_pct ~ colData(se)$batch, main="percent mito genes", col=colb, las=2, cex.axis=0.7)
+plot(qc$ribo_pct, col=colb[batch], xlab="cell index", main="% ribo (Rpl*) genes"); legend("topleft", legend=levels(batch), fill=colb, cex=0.8)
+boxplot(qc$ribo_pct ~ colData(se)$batch, main="percent ribo genes", col=colb, las=2, cex.axis=0.7)
+dev.off()
 
-screeplot(qcpca, type = "lines", main = "QC-PCA screeplot")
-plot(qcpca$x, col=alpha(colb[batch], 0.3), pch=19, main="QC PCA")
-legend("topleft", legend=levels(batch), fill=colb, cex=0.5)
+message(paste0("Percent total variance captured in first 10 expression PCs is ", round(cumsum(pca$sdev^2/sum(pca$sdev^2))[10] * 100, digits = 2)))
 
+pdf(file=file.path(vizdir, paste0(exptstr, "_QCpca_prefilt.pdf")), length=11, width=8.5)
 screeplot(pca, type = "lines", npcs = 50, main = "Expression PCA screeplot")
-plot(pca$x, col=alpha(colb[batch], 0.3), pch=19, cex=0.4, main="simple filtering, scaling --> PCA")
-legend("topright", legend=levels(batch), fill=colb, cex=0.5)
-
-print(paste0("Percent total variance captured in first 10 expression PCs is ", round(cumsum(pca$sdev^2/sum(pca$sdev^2))[10] * 100, digits = 2)))
+screeplot(qcpca, type = "lines", main = "QC-PCA screeplot")
 
 fig_data <- data.frame(pca$x[,1:2], qc,
-                       QPC1=qcpca$x[,1], QPC2=qcpca$x[,2])
+                       QPC1=qcpca$x[,1], QPC2=qcpca$x[,2], batch = batch) 
 
-fig_pca <- ggplot(fig_data, aes(x = PC1, y = PC2, color = log10_total_counts)) +
-  geom_point() + scale_color_continuous(low = "blue", high = "yellow")
-fig_pca
+ggplot(fig_data, aes(x = PC1, y = PC2, color = batch)) +
+  geom_point(alpha = 0.3) + scale_color_manual(values = colb) + ggtitle("Expression PCA with simple filtering and scaling")
+ggplot(fig_data, aes(x = QPC1, y = QPC2, color = batch)) +
+  geom_point(alpha = 0.3) + scale_color_manual(values = colb) + ggtitle("QC metric PCA")
 
-fig_qpca <- ggplot(fig_data, aes(x = QPC1, y = QPC2, color = log10_total_counts)) +
-  geom_point() + scale_color_continuous(low = "blue", high = "yellow")
-fig_qpca
+invisible(list("log10_total_counts", "ribo_pct", "mito_pct", "log10_total_features"), function(feature) {
+  pcaggplot(fig_data, "Expression PCA", feature)
+  pcaggplot(fig_data, "QC metric PCA", feature)
+})
 
-fig_pca <- ggplot(fig_data, aes(x = PC1, y = PC2, color = ribo_pct)) +
-  geom_point() + scale_color_continuous(low = "blue", high = "yellow")
-fig_pca
-
-fig_qpca <- ggplot(fig_data, aes(x = QPC1, y = QPC2, color = ribo_pct)) +
-  geom_point() + scale_color_continuous(low = "blue", high = "yellow")
-fig_qpca
-
-fig_pca <- ggplot(fig_data, aes(x = PC1, y = PC2, color = mito_pct)) +
-  geom_point() + scale_color_continuous(low = "blue", high = "yellow")
-fig_pca
-
-fig_qpca <- ggplot(fig_data, aes(x = QPC1, y = QPC2, color = mito_pct)) +
-  geom_point() + scale_color_continuous(low = "blue", high = "yellow")
-fig_qpca
-
-fig_pca <- ggplot(fig_data, aes(x = PC1, y = PC2, color = log10_total_features)) +
-  geom_point() + scale_color_continuous(low = "blue", high = "yellow")
-fig_pca
-
-fig_qpca <- ggplot(fig_data, aes(x = QPC1, y = QPC2, color = log10_total_features)) +
-  geom_point() + scale_color_continuous(low = "blue", high = "yellow")
-fig_qpca
-
+dev.off()
 
 
 # Filtering
