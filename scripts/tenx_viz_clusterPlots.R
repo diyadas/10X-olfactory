61ec2405449b8e7e547fe78e9dcad3e855cfe527
Diya Das <diyadas@users.noreply.github.com>
2018-07-23 16:14:15 -0700

    fixed bugs, added t-SNEs

--- scripts/tenx_viz_clusterPlots.R
+++ scripts/tenx_viz_clusterPlots.R
@@ -28,19 +28,51 @@ source("tenx_helper.R")
 load(file.path(datdir, pasteu(exptstr, opt$method, "snn.Rda")))
 load(file.path(datdir, pasteu(exptstr, "se_filtered.Rda")))
 
+
+names(batch) <- colnames(seu@data)
+names(expt) <- colnames(seu@data)
 clus.labels <- sort(clus.labels)
-batch <- batch[clus.labels]
-expt <- expt[clus.labels]
+batch <- batch[names(clus.labels)]
+expt <- expt[names(clus.labels)]
 
 seed <- 2782472
 
 # heatmaps
 
-oe_markers <- intersect(unlist(read.table("../ref/oe_markers32+regen.txt")),rownames(seu@data))
-breakv <- c(min(seu@data), seq(0, quantile(seu@data[seu@data > 0], .99, na.rm = TRUE), length = 50), max(seu@data))
+oe_markers <- intersect(unlist(read.table("../ref/oe_markers32+regen.txt")), 
+                        rownames(seu@data))
+breakv <- c(min(seu@data), 
+            seq(0, quantile(seu@data[seu@data > 0], .99, na.rm = TRUE), length = 50), 
+            max(seu@data))
 
-pdf(file = file.path(vizdir, pasteu(exptstr, "selectmarker", Sys.Date(), ".pdf")), width = 6, height = 5.5)
-plotHeatmap(seu@data[oe_markers, names(clus.labels)], clusterSamples = FALSE, clusterFeatures = FALSE, breaks = breakv, colData = data.frame(cluster = clus.labels, expt = expt, batch = batch), clusterLegend = list(expt = cole), annLegend = TRUE)
+pdf(file = file.path(vizdir, pasteu(exptstr, "marker", Sys.Date(), ".pdf")), 
+    width = 6, height = 5.5)
+plotHeatmap(seu@data[oe_markers, names(clus.labels)], clusterSamples = FALSE, clusterFeatures = FALSE, breaks = breakv, colData = data.frame(cluster = clus.labels, expt = expt, batch = batch), clusterLegend = list(cluster = bigPalette, expt = cole), annLegend = TRUE)
 dev.off()
 
 # t-SNE colored by cluster and time point
+rtsne_fx <- function(cmobj, clusters, ngenes = 500) {
+  set.seed(9887)
+  vars <- apply(seu@data[,names(clusters)], 1, var)
+  vars <- sort(vars, decreasing = TRUE)
+  var_data <- seu@data[names(vars)[1:ngenes],]
+  tsne_data <- Rtsne(t(var_data[,names(clusters)]), 
+                     perplexity = 10, max_iter = 1000)
+  return(tsne_data)
+}
+
+tsne_data <- rtsne_fx(seu, clus.labels, ngenes = 500)
+pdf(file = file.path(vizdir, pasteu(exptstr, "tsne", "clus", method, opt$norm, Sys.Date(), ".pdf")))
+plot(tsne_data$Y, pch = 19, cex = 0.4, col = bigPalette[clus.labels], xlab = "TSNE 1", ylab = "TSNE 2")
+legend("topleft", legend = levels(clus.labels), fill = bigPalette, cex = 0.5)
+dev.off()
+
+pdf(file = file.path(vizdir, pasteu(exptstr, "tsne", "batch", method, opt$norm, Sys.Date(), ".pdf")))
+plot(tsne_data$Y, pch = 19, cex = 0.4, col = colb[batch], xlab = "TSNE 1", ylab =" TSNE 2")
+legend("bottomleft", legend = levels(batch), fill = colb, cex = 0.6)
+dev.off()
+
+pdf(file = file.path(vizdir, pasteu(exptstr, "tsne", "expt", method, opt$norm, Sys.Date(), ".pdf")))
+plot(tsne_data$Y, pch = 19, cex = 0.4, col = cole[expt], xlab = "TSNE 1", ylab =" TSNE 2")
+legend("bottomleft", legend = levels(expt), fill = cole, cex = 0.6)
+dev.off()
