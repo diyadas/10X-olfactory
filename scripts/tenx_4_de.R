0db3df3a0650822051a6695d76e3bb79fdafb8fe
Diya Das <diyadas@users.noreply.github.com>
2018-10-15 23:23:33 -0700

    preliminary DE script

---
+++ scripts/tenx_4_de.R
@@ -0,0 +1,38 @@
+# Differential gene expression of 10X data
+# Authors: Diya Das
+# Last revised: Mon Oct 15 22:19:11 2018
+
+# Load command-line arguments
+library(Seurat)
+library(clusterExperiment)
+library(BiocParallel)
+library(optparse)
+
+option_list <- list(
+  make_option("--expt", default = "", type = "character", help = "Experiment ID"),
+  make_option("--normalization", type = "character", help = "name of normalization"),
+  make_option("--method", type = "character", help = "scone or zinb"),
+  make_option("--ncores", default = "1", type = "double")
+)
+
+opt <- parse_args(OptionParser(option_list = option_list))
+exptstr <- opt$expt
+method <- opt$method
+outdir <- file.path("../output", exptstr, "data")
+ncores <- opt$ncores
+
+register(MulticoreParam(workers = opt$ncores))
+
+source("tenx_helper.R")
+
+load(file.path(outdir, pasteu(exptstr, opt$method, "snn.Rda")))
+
+ce <- ClusterExperiment(seu@raw.data, clusters = clus.labels, 
+                        transformation = function(x) log2(x + 1))
+ce <- makeDendrogram(ce, reduceMethod = "var", nDims = 1000)
+de_ce <- getBestFeatures(ce, contrastType = "Pairs", whichAssay = 1, 
+                         DEMethod = "limma", number = 100)
+write.table(de_ce, file = file.path(outdir, 
+                                    pasteu(exptstr, method, 
+                                           normalization,"DE_Pairs.txt")),
+            sep = "\t", quote = FALSE, row.names = FALSE)
