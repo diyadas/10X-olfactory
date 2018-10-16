cc2c48a57b53926bdd1ea81e8514573c9f9ef166
Diya Das <diyadas@users.noreply.github.com>
2018-10-15 17:45:54 -0700

    shell scripts

---
+++ scripts/tenx_2a_scone_1_eval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -V
+#$ -j y
+#$ -S /bin/bash
+#$ -M diyadas@berkeley.edu,rchance@berkeley.edu
+#$ -m beas
+#
+
+ncores=$1
+NOW=$(date +"_%m%d%Y-%H%M%S")
+
+R_LIBS=/share/groups/diya-russell/rpack/3.5/ R --vanilla < tenx_2a_scone_1_eval.R --args --expt regen --ncores $ncores --subsample TRUE > 'tenx_2a_scone_1_eval'$NOW'.Rout'
---
+++ scripts/tenx_3b_snn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -V
+#$ -j y
+#$ -S /bin/bash
+#$ -M diyadas@berkeley.edu,rchance@berkeley.edu
+#$ -m beas
+#
+
+ncores=$1
+NOW=$(date +"_%m%d%Y-%H%M%S")
+
+R_LIBS=/share/groups/diya-russell/rpack/3.5/ R --vanilla < tenx_3b_snn.R --args --expt regen --ncores $ncores --normalization $2 --method scone > 'tenx_3n_snn'$NOW'.Rout'
