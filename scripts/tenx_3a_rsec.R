b9d3961d12392e96be5a5411fca6c0796a2ff475
Diya Das <diyadas@users.noreply.github.com>
2018-10-15 17:24:51 -0700

    update to RSEC code

--- scripts/tenx_3a_rsec.R
+++ scripts/tenx_3a_rsec.R
@@ -12,7 +12,9 @@ option_list <- list(
   make_option("--expt", default = "", type = "character", help = "Experiment ID"),
   make_option("--normalization", type = "character", help = "name of normalization"),
   make_option("--method", type = "character", help = "scone or zinb"),
-  make_option("--ncores", default = "1", type = "double")
+  make_option("--ncores", default = "1", type = "double"),
+  make_option("--sequential", default = FALSE, type = "logical", help = "TRUE or FALSE"),
+  make_option("--subsample", default = FALSE, type = "logical", help = "TRUE or FALSE")
 )
 
 opt <- parse_args(OptionParser(option_list = option_list))
@@ -21,9 +23,11 @@ method <- opt$method
 outdir <- file.path("../output", exptstr, "data")
 ncores <- opt$ncores
 
-register(MulticoreParam(workers = opt$ncores))
+#register(MulticoreParam(workers = opt$ncores))
 
 source("tenx_helper.R")
+
+message(str(args))
 
 load(file.path(outdir, pasteu(exptstr, method, "data.Rda")))
 if (method == "scone") {
@@ -32,11 +36,24 @@ if (method == "scone") {
    gc()
 }
 seed <- 2782472
-cl <- clusterMany(mat, isCount = TRUE, reduceMethod = "PCA", nReducedDims = 30,
-                  alphas = 0.1,
-                  sequential = TRUE, subsample = TRUE, minSizes = 5,
-                  clusterFun = c("hierarchical01"), subsampleArgs = list(largeDataset = TRUE),
-                  ks = 4:12, random.seed = seed,
-                  ncores = ncores)
 
-save(cl, file = file.path(outdir, pasteu(exptstr, "_scone_rsec.Rda")))
+cl <- RSEC(mat, k0s = seq(10,50,by=5), alphas = c(0.1,0.3),
+                reduceMethod = "PCA",
+                transFun = function(x) log2(x+1), # should override isCount=FALSE, or could write isCount=TRUE
+                nReducedDims = 30,
+                sequential = opt$sequential, subsample = opt$subsample,
+                subsampleArgs = list(resamp.num = 50, clusterFunction="kmeans"),
+                betas = c(0.8),
+                clusterFunction = "kmeans",  
+		minSizes = 1,
+                ncores = ncores,
+                consensusProportion = 0.7, consensusMinSize = 10, # with sequential turned off, then consensus might result in many -1s
+                dendroReduce = "PCA", dendroNDims=30,
+                mergeMethod = "adjP", mergeCutoff=0.1,
+                mergeLogFCcutoff = 1, random.seed=2357891,
+                mergeDEMethod = "limma-voom", 
+                verbose = TRUE, run = TRUE)
+
+subsamplestr <- ifelse (opt$subsample, "sub", "nosub")
+seqstr <- ifelse(opt$sequential, "seq", "noseq")
+save(cl, file = file.path(outdir, pasteu(exptstr, opt$method,"rsec", subsamplestr, seqstr,".Rda")))
---
+++ scripts/tenx_3a_rsec.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -V
+#$ -j y
+#$ -S /bin/bash
+#$ -M diyadas@berkeley.edu,rchance@berkeley.edu
+#$ -m beas
+#
+
+ncores=$1
+NOW=$(date +"_%m%d%Y-%H%M%S")
+
+while true; do free -h >> 'tenx_3_rsec'$NOW'_memory.out'; sleep 15; done &
+
+R_LIBS=/share/groups/diya-russell/rpack/3.5/ R --vanilla < tenx_3a_rsec.R --args --expt regen --ncores $ncores --normalization $2 --method scone > 'tenx_3a_rsec'$NOW'.Rout' 2>&1 
