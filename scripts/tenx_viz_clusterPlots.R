fe4bbea26d8ec44bbee549799a82c228143607af
Diya Das <diyadas@users.noreply.github.com>
2018-10-31 09:48:50 -0700

    update tSNE method

--- scripts/tenx_viz_clusterPlots.R
+++ scripts/tenx_viz_clusterPlots.R
@@ -1,6 +1,6 @@
-# Clustering of 10X data using SNN (Seurat)
-# Authors: Russell Fletcher, Diya Das, and Rebecca Chance
-# Last revised: Thu Jul 19 13:46:01 2018
+# Visualization of 10X data
+# Authors: Diya Das and Rebecca Chance
+# Last revised: Wed Oct 31 09:35:01 2018
 
 # Load command-line arguments
 library(scone)
@@ -29,11 +29,10 @@ source("tenx_helper.R")
 load(file.path(datdir, pasteu(exptstr, opt$method, "snn.Rda")))
 load(file.path(datdir, pasteu(exptstr, "se_filtered.Rda")))
 
-names(batch) <- colnames(seu@data)
-names(expt) <- colnames(seu@data)
-clus.labels <- sort(clus.labels)
-batch <- batch[names(clus.labels)]
-expt <- expt[names(clus.labels)]
+metadata <- data.frame(seu@meta.data, expt = expt, batch = batch,  
+	    	      samples = colnames(seu@data), row.names = "samples")
+metadata <- metadata[with(metadata, order(res.0.5, expt, batch)),]
+metadata <- metadata[, c('res.0.5', 'expt', 'batch')]
 
 seed <- 2782472
 
@@ -44,35 +43,38 @@ oe_markers <- intersect(unlist(read.table("../ref/oe_markers32+regen.txt")),
 breakv <- c(min(seu@data), 
             seq(0, quantile(seu@data[seu@data > 0], .99, na.rm = TRUE), length = 50), 
             max(seu@data))
-
-pdf(file = file.path(vizdir, pasteu(exptstr, "markerhm", method, opt$norm, Sys.Date(), ".pdf")), 
+breakv <- unique(breakv)
+pdf(file = file.path(vizdir, pasteu(exptstr, "markerhm", method, opt$norm, format(Sys.time(), "%Y%m%d_%H%M%S"), ".pdf")), 
     width = 6, height = 5.5)
-plotHeatmap(seu@data[oe_markers, names(clus.labels)], clusterSamples = FALSE, clusterFeatures = FALSE, breaks = breakv, colData = data.frame(cluster = clus.labels, expt = expt, batch = batch), clusterLegend = list(cluster = bigPalette, expt = cole), annLegend = TRUE)
+plotHeatmap(seu@data[oe_markers, rownames(metadata)], clusterSamples = FALSE, clusterFeatures = FALSE, breaks = breakv, colData = metadata, 
+				 clusterLegend = list(res.0.5 = bigPalette, expt = cole, batch = bigPalette), annLegend = TRUE)
 dev.off()
 
 # t-SNE colored by cluster and time point
-rtsne_fx <- function(cmobj, clusters, ngenes = 500) {
-  set.seed(9887)
-  vars <- apply(seu@data[,names(clusters)], 1, var)
-  vars <- sort(vars, decreasing = TRUE)
-  var_data <- seu@data[names(vars)[1:ngenes],]
-  tsne_data <- Rtsne(t(var_data[,names(clusters)]), 
-                     perplexity = 10, max_iter = 10000)
-  return(tsne_data)
+
+
+if (is.null(seu@dr$tsne@cell.embeddings)){
+ngenes = 500
+vars <- apply(seu@data, 1, var)
+vars <- sort(vars, decreasing = TRUE)
+genes.use <- names(vars)[1:ngenes]
+
+seu <- RunTSNE(seu, reduction.use = "pca", dims.use = 1:50,
+  genes.use = genes.use, seed.use = seed, tsne.method = "Rtsne", perplexity = 10, max_iter = 10000,
+  dim.embed = 2, reduction.name = "tsne")
 }
 
-tsne_data <- rtsne_fx(seu, clus.labels, ngenes = 500)
-pdf(file = file.path(vizdir, pasteu(exptstr, "tsne", "clus", method, opt$norm, Sys.Date(), ".pdf")))
-plot(tsne_data$Y, pch = 19, cex = 0.4, col = bigPalette[clus.labels], xlab = "TSNE 1", ylab = "TSNE 2")
-legend("topleft", legend = levels(clus.labels), fill = bigPalette, cex = 0.5)
+pdf(file = file.path(vizdir, pasteu(exptstr, "tsne", "clus", method, opt$norm, format(Sys.time(), "%Y%m%d_%H%M%S"), ".pdf")))
+plot(tsne_data$Y, pch = 19, cex = 0.4, col = bigPalette[factor(seu@meta.data[,"res.0.5"])], xlab = "TSNE 1", ylab = "TSNE 2")
+legend("topleft", legend = levels(factor(seu@meta.data[,"res.0.5"])), fill = bigPalette, cex = 0.5)
 dev.off()
 
-pdf(file = file.path(vizdir, pasteu(exptstr, "tsne", "batch", method, opt$norm, Sys.Date(), ".pdf")))
+pdf(file = file.path(vizdir, pasteu(exptstr, "tsne", "batch", method, opt$norm, format(Sys.time(), "%Y%m%d_%H%M%S"), ".pdf")))
 plot(tsne_data$Y, pch = 19, cex = 0.4, col = colb[batch], xlab = "TSNE 1", ylab =" TSNE 2")
 legend("bottomleft", legend = levels(batch), fill = colb, cex = 0.6)
 dev.off()
 
-pdf(file = file.path(vizdir, pasteu(exptstr, "tsne", "expt", method, opt$norm, Sys.Date(), ".pdf")))
+pdf(file = file.path(vizdir, pasteu(exptstr, "tsne", "expt", method, opt$norm, format(Sys.time(), "%Y%m%d_%H%M%S"), ".pdf")))
 plot(tsne_data$Y, pch = 19, cex = 0.4, col = cole[expt], xlab = "TSNE 1", ylab =" TSNE 2")
 legend("bottomleft", legend = levels(expt), fill = cole, cex = 0.6)
 dev.off()
