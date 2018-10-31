590698ad74e27b34475c76a5bf9bac967f5439f7
Diya Das <diyadas@users.noreply.github.com>
2018-10-31 09:28:31 -0700

    update to OneAgainstAll DE and for particular resolution

--- scripts/tenx_4_de.R
+++ scripts/tenx_4_de.R
@@ -27,12 +27,22 @@ source("tenx_helper.R")
 
 load(file.path(outdir, pasteu(exptstr, opt$method, "snn.Rda")))
 
-ce <- ClusterExperiment(seu@raw.data, clusters = clus.labels, 
+ce <- ClusterExperiment(seu@raw.data, clusters = seu@meta.data[,"res.0.5"], 
                         transformation = function(x) log2(x + 1))
 ce <- makeDendrogram(ce, reduceMethod = "var", nDims = 1000)
-de_ce <- getBestFeatures(ce, contrastType = "Pairs", whichAssay = 1, 
+
+de_ce <- getBestFeatures(ce, contrastType = "OneAgainstAll", whichAssay = 1, 
                          DEMethod = "limma", number = 100)
 write.table(de_ce, file = file.path(outdir, 
                                     pasteu(exptstr, method, 
-                                           normalization,"DE_Pairs.txt")),
+                                           opt$normalization,"res05","DE_OneAgainstAll", format(Sys.time(), "%Y%m%d_%H%M%S"),".txt")),
             sep = "\t", quote = FALSE, row.names = FALSE)
+
+
+#de_ce <- getBestFeatures(ce, contrastType = "Pairs", whichAssay = 1, 
+#                         DEMethod = "limma", number = 100)
+#write.table(de_ce, file = file.path(outdir, 
+#                                    pasteu(exptstr, method, 
+#                                           opt$normalization,"DE_Pairs", format(Sys.time(), "%Y%m%d_%H%M%S"),".txt")),
+#            sep = "\t", quote = FALSE, row.names = FALSE)
+
