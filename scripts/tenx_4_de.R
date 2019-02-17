226509dd17942ac6cb2f8a74fd97204d5c1116f4
Rebecca Chance <rchance@berkeley.edu>
2019-02-17 15:25:05 -0800

    clusters from seu@metadata to ce obj

--- scripts/tenx_4_de.R
+++ scripts/tenx_4_de.R
@@ -36,7 +36,7 @@ metadata <- data.frame(seu@meta.data, expt = colData(se_filtered)$expt,
                       samples = colnames(seu@data), row.names = "samples")
 metadata <- metadata[colnames(seu@raw.data),]
 
-ce <- ClusterExperiment(seu@raw.data, clusters = metadata[,"res.0.5"], 
+ce <- ClusterExperiment(seu@raw.data, clusters = seu@meta.data[,"res.0.5"], 
                         transformation = function(x) log2(x + 1))
 colData(ce) <- DataFrame(metadata)
 
