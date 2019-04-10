7c2cc85e8738f94759428e45e80a8f768d89e4e6
Diya Das <diyadas@users.noreply.github.com>
2019-04-09 21:48:57 -0700

    update cellranger count for output of bcl2fastq

--- crscripts/cellranger-count.sh
+++ crscripts/cellranger-count.sh
@@ -8,6 +8,12 @@
 #$ -m beas
 
 NOW=$(date +'%Y%M%D_%H%M%S')
-sample=$1
 
-cellranger count --id=$sample --sample=$sample --fastqs=$SHARE/10X-olfactory/output/cellranger/HWVK5BBXX/outs/fastq_path/HWVK5BBXX --transcriptome=$SHARE/annotations/GRCm38p4Mm10
+#cellranger count --id=$sample --sample=$sample --fastqs=$SHARE/10X-olfactory/output/cellranger/HWVK5BBXX/outs/fastq_path/HWVK5BBXX --transcriptome=$SHARE/annotations/GRCm38p4M10
+#FASTQPATH="/share/groups/diya-russell/10X-olfactory/output/seq/Stafford/RBP4B"
+#FASTQPATH="/share/groups/diya-russell/10X-olfactory/output/seq/190315_100PE_HS4K2B"
+#FASTQPATH="/share/groups/diya-russell/10X-olfactory/output/seq/190322_100PE_HS4K2A" #wholeOE mini injury timecourse
+FASTQPATH="/share/groups/diya-russell/10X-olfactory/output/seq/regen_whOE_batch2" #wholeOE mini injury timecourse batch2
+cellranger count --id=$1 --sample=$2 --fastqs=$FASTQPATH --transcriptome=$SHARE/annotations/GRCm38p4Mm10
+
+#id regen_24HPI-2 this is a folder -1 is RF  -2 is RKC   sample SI-GA-G4-a  
