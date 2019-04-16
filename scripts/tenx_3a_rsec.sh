cb65c982924693b30f277c6695cb70d6e6d1bb4b
Rebecca Chance <rchance@berkeley.edu>
2019-04-16 10:21:29 -0700

    expect num arguments now 3

--- scripts/tenx_3a_rsec.sh
+++ scripts/tenx_3a_rsec.sh
@@ -17,7 +17,7 @@ idfilt="FALSE"
 
 job=$(basename "$0")
 NOW=$(date +"%Y%m%d-%H%M%S")
-LOG="logs/${expt}_2a_scone_2_compute_${job}_${NOW}.Rout"
+LOG="logs/${expt}_3a_rsec_${job}_${NOW}.Rout"
 exec >> "$LOG" 2>&1 || exit 1     # redirect stdout and error to log file, will fail if the logs directory doesn't exist
 
 # as refactored by JW Adams from last commit
@@ -35,7 +35,7 @@ usage() {
        exit 2
 }
 
-[[ $# -eq 2 ]] || usage  # fail if incorrect number of args and print usage info
+[[ $# -eq 3 ]] || usage  # fail if incorrect number of args and print usage info
 
 
 while true; do free -h >> 'tenx_3a_rsec_'$NOW'_memory.out'; sleep 15; done &
@@ -44,4 +44,4 @@ while true; do free -h >> 'tenx_3a_rsec_'$NOW'_memory.out'; sleep 15; done &
 run tenx_3a_rsec.R \
    env R_LIBS=/share/groups/diya-russell/rpack/3.5/ R --vanilla --args \
        --expt "$expt" --normalization "$normalization" \
-       --ncores "$ncores" --method "$method" 
+       --ncores "$ncores" --method "$method" --idfilt "$idfilt"
