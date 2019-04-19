93496043409fa2942ceac310806d61179515eaa8
Rebecca Chance <rchance@berkeley.edu>
2019-04-19 13:05:27 -0700

    outdir to datdir

--- scripts/tenx_2b_zinb.R
+++ scripts/tenx_2b_zinb.R
@@ -7,13 +7,12 @@ rm(list = ls())
 options(getClass.msg = FALSE)
 library(optparse)
 option_list <- list(
-  make_option("--expt", type = "character", help = "Experiment ID"),
+  make_option("--expt", default = "", type = "character", help = "Experiment ID"),
   make_option("--ncores", default = "1", type = "double"),
   make_option("--idfilt", type = "logical", help = "logical, has sample ID filtering been performed?")
 )
 
 opt <- parse_args(OptionParser(option_list = option_list))
-print(opt)
 exptstr <- opt$expt
 datdir <- file.path("../output", exptstr, "data")
 vizdir <- file.path("../output", exptstr, "viz")
@@ -50,13 +49,13 @@ vars <- sort(vars, decreasing = TRUE)
 
 zinb <- zinbFit(se_filtered[names(vars)[1:1000],], X = "~ log10_total_counts + pct_counts_in_top_500_features + ribo_pct", K = 20, epsilon = 1000)
 zinbparams <- "zinbFit(se_filtered[names(vars)[1:1000],], X = '~ log10_total_counts + pct_counts_in_top_500_features + ribo_pct', K = 20, epsilon = 1000)"
-save(zinb, zinbparams, file = file.path(outdir, pasteu(exptstr,"zinb_data.Rda")))
+save(zinb, zinbparams, file = file.path(datdir, pasteu(exptstr,"zinb_data.Rda")))
 
 W <- getW(zinb)
 d <- dist(W)
 
-save(W, zinbparams, file = file.path(outdir, pasteu0(exptstr, "zinbW", format(Sys.time(), "%Y%m%d_%H%M%S"), ".Rda")))
+save(W, zinbparams, file = file.path(datdir, pasteu0(exptstr, "zinbW", format(Sys.time(), "%Y%m%d_%H%M%S"), ".Rda")))
 
 zinb_obj <- zinbwave(se_filtered[names(vars)[1:1000],], fitted_model = zinb, K = 20, epsilon = 1000)
 save(zinb_obj, zinbparams, zinb, 
-       file = file.path(outdir, pasteu(exptstr,idfiltstr, "zinb_data.Rda")))
+       file = file.path(datdir, pasteu(exptstr,idfiltstr, "zinb_data.Rda")))
