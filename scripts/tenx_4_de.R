16ba6b7f8de4a58c325305a4bd326df9c6fcf1c3
rkchance <rchance@berkeley.edu>
2019-05-09 02:56:21 -0400

    rsec mergecutoff load

--- scripts/tenx_4_de.R
+++ scripts/tenx_4_de.R
@@ -12,6 +12,8 @@ option_list <- list(
   make_option("--expt", default = "", type = "character", help = "Experiment ID"),
   make_option("--normalization", type = "character", help = "name of normalization"),
   make_option("--method", type = "character", help = "scone or zinb"),
+  make_option("--clusmethod", type = "character", default = "snn",
+              help = "clustering method - snn or rsec"),
   make_option("--ncores", default = "1", type = "double"),
   make_option("--idfilt", default = FALSE, type = "logical", help = "logical, has sample ID filtering been performed?")
 )
@@ -21,6 +23,7 @@ exptstr <- opt$expt
 method <- opt$method
 datdir <- file.path("../output", exptstr, "data")
 ncores <- opt$ncores
+clusmethod <- opt$clusmethod
 
 print(opt)
 
@@ -33,10 +36,42 @@ if (opt$idfilt) {
 register(MulticoreParam(workers = opt$ncores))
 
 source("tenx_helper.R")
+if (opt$normalization == "scone") {
+datfiles <<- list.files(path = datdir, pattern = pasteu0(exptstr, method, opt$normalization, 
+                                                        opt$clusmethod, idfiltstr), full.names = TRUE)
+datfile <- datfiles[length(datfiles)]
+print(paste("Loading this data file: ", datfile))
+load(datfile)
+} else if (opt$normalization == "zinb") {
+datfiles <<- list.files(path = datdir, pattern = pasteu0(exptstr, method, "data", idfiltstr), full.names = TRUE)
+datfile <- datfiles[length(datfiles)]
+print(paste("Loading this data file: ", datfile))
+load(datfile)
+}
+load(file.path(datdir, pasteu0(exptstr, "1_se_filtqc", idfiltstr, ".Rda")))
+
+
 datfiles <- list.files(path = datdir, pattern = pasteu(exptstr, method, opt$normalization, "snn", idfiltstr), full.names = TRUE)
 datfile <- datfiles[length(datfiles)]
 print(paste("Loading this data file: ", datfile))
 load(datfile)
+
+if (opt$clusmethod == "rsec") {
+if (opt$method == "scone") {
+#ob_scone_none,fq,qc_k=2,no_bio,batch_rsec_locfdr_mergecutoff_0.5_20190425_155428.Rda
+datfiles <- list.files(path = datdir, 
+                       pattern = pasteu(exptstr, method, opt$normalization, clusmethod, "locfdr_mergecutoff_0.08"), full.names = TRUE)
+datfile <- datfiles[length(datfiles)]
+print(paste("Loading this data file: ", datfile))
+load(datfile)
+} else if (opt$method == "zinb") {
+datfiles <- list.files(path = datdir, 
+                       pattern = pasteu(exptstr, method, opt$normalization, clusmethod, "locfdr_mergecutoff_0.3"), full.names = TRUE)
+datfile <- datfiles[length(datfiles)]
+print(paste("Loading this data file: ", datfile))
+load(datfile)
+}
+}
 
 load(file.path(datdir, pasteu0(exptstr, "1_se_filtqc", idfiltstr, ".Rda")))
 se_filtered <- se_filtered[, colnames(seu@data)]
@@ -44,6 +79,12 @@ metadata <- data.frame(seu@meta.data, expt = colData(se_filtered)$expt,
                                       batch = colData(se_filtered)$batch,
                       samples = colnames(seu@data), row.names = "samples")
 metadata <- metadata[colnames(seu@raw.data),]
+
+if (opt$clusmethod == "rsec") {
+ce <- ClusterExperiment(cl2, clusters = primaryCluster)
+de_onevall <- getBestFeatures(cl2, contrastType = "OneAgainstAll", whichAssay = 1,
+                         DEMethod = "limma", number = 100)
+} else if (opt$clusmethod == "snn") {
 
 #ce <- ClusterExperiment(seu@raw.data, clusters = seu@meta.data[,"res.0.5"], 
 #                        transformation = function(x) log2(x + 1))
@@ -53,11 +94,11 @@ colData(ce) <- DataFrame(metadata)
 
 ce <- makeDendrogram(ce, reduceMethod = "var", nDims = 1000)
 
-de_ce <- getBestFeatures(ce, contrastType = "OneAgainstAll", whichAssay = 1, 
+de_ce <- getBestFeatures(ce, contrastType = "OneAgainstAll", whichAssay = 1,
                          DEMethod = "limma", number = 100)
-
 rownames(se_filtered) <- rowData(se_filtered)$Symbol
 colData(ce)$batch <- colData(se_filtered)$batch
+}
 
 save(ce, file = file.path(datdir, pasteu0(exptstr, method,
                                            opt$normalization,"res05","cmobj", format(Sys.time(), "%Y%m%d_%H%M%S"),".Rda")))
