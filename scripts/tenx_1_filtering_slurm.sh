c168771dbda8e3413d5a35809bb93cc14c9434a5
rkchance <rchance@berkeley.edu>
2019-05-14 20:01:15 -0400

    for slurm filtering

---
+++ scripts/tenx_1_filtering_slurm.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+#
+#SBATCH --export=ALL
+#SBATCH --nodes=1
+#SBATCH -t 48:00:00
+#SBATCH -p LM
+#SBATCH --mem 500GB
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=diyadas@berkeley.edu,rchance@berkeley.edu
+
+module load gcc
+
+ncores=$1
+#expt="cortex"
+#aggr="Rbp4AB"
+#exptinfo="cortex_exptinfo.csv"
+#posctrlfile="../ref/cortexgenes.txt"
+
+#expt="ob"
+#aggr="RCOB2AB567"
+#exptinfo="rcob_exptinfo.csv"
+#posctrlfile="../ref/OBmarkers.txt"
+#hkfile="hkpackage"
+
+expt="regen"
+aggr="regen_20190325"
+exptinfo="regen_exptinfo.csv"
+posctrlfile="../ref/oeRegPosCon.txt"
+hkfile="../ref/hkl615.txt"
+
+runQC="TRUE"
+#exclude="" #input ref/expt_SOMETHING_exclude.Rda typically gene names or putative identity ex ob_AON or ob_Lmo3_Enc1
+idfilt="FALSE"
+
+job=$SLURM_JOB_ID
+NOW=$(date +"%Y%m%d-%H%M%S")
+LOG="logs/${expt}_1_filtering_slurm_${job}_${NOW}.Rout"
+exec >> "$LOG" 2>&1 || exit 1     # redirect stdout and error to log file, will fail if the logs directory doesn't exist
+
+# as refactored by JW Adams from last commit
+# usage: run input_file command to run
+run() {
+       local input="$1"
+       shift;  # remove file from argument list
+       echo "command: $* < $input"
+       echo "--------------"
+       "$@" < "$input"
+}
+
+usage() {
+       echo "usage: tenx_1_filtering_slurm.sh ncores" >&2
+       exit 2
+}
+
+[[ $# -eq 1 ]] || usage  # fail if incorrect number of args and print usage info
+
+while true; do free -h >> 'memorylogs/tenx_1_filtering_slurm_'$NOW'_memory.out'; sleep 15; done &
+
+run tenx_1_filtering.R \
+   env R_LIBS=/pylon5/ib5phhp/shared/rpack/3.5/ R --vanilla --args \
+       --expt "$expt" --ncores "$ncores" --aggr "$aggr" \
+       --exptinfo "$exptinfo" \
+       --hkfile "$hkfile" \
+       --idfilt "$idfilt" \
+       --posctrlfile "$posctrlfile" \
+       --runQC "$runQC" \
+
+
+#R_LIBS=/share/groups/diya-russell/rpack/3.5/ R --vanilla < tenx_1_filtering.R --args \
+#--expt $EXPT --ncores $ncores --aggr reg_whOE \
+#--exptinfo regen_whoe_exptinfo.csv \
+#--runQC TRUE \
+#--exclude double \
+#> logs/$EXPT'_tenx_1_filtering'$NOW'.Rout' 2>&1
+
+#R_LIBS=/share/groups/diya-russell/rpack/3.5/ R --vanilla < tenx_1_filtering.R --args \
+#--expt $EXPT --ncores $ncores --aggr RCOB2AB567 \
+#--exptinfo rcob_exptinfo.csv \
+#--hkfile hkpackage \
+#--posctrlfile "../ref/OBmarkers.txt" \
+#--runQC TRUE \
+#--exclude aon \
+#> logs/$EXPT'_tenx_1_filtering'$NOW'.Rout' 2>&1
+
+# R_LIBS=/share/groups/diya-russell/rpack/3.5/ R --vanilla < tenx_1_filtering.R --args \
+# --expt $EXPT  --ncores $ncores --aggr Rbp4AB \
+# --exptinfo cortex_exptinfo.csv \
+# --hkfile hkpackage \
+# --posctrlfile "../ref/cortexgenes.txt" \
+# --runQC TRUE \
+# #--exclude gad1 \
+# > logs/$EXPT'_tenx_1_filtering'$NOW'.Rout' 2>&1
+
+
+
+# option_list <- list(
+#   make_option("--expt", type = "character", help = "Experiment ID"),
+#   make_option("--ncores", default = "1", type = "double"),
+#   make_option("--aggr", type = "character",
+#               help = "name of aggregate ID/directory"),
+#   make_option("--exptinfo", type = "character",
+#               help = "text file with list of cellranger experiments, from 
+#               aggregate...csv, expanded with columns for expt and batch: \n 
+#               column 1: library_id, column 3: expt, column 4: batch"),
+#   make_option("--annotation", default = "GRCm38p4Mm10", type = "character",
+#               help = "name of genomic reference used by cellranger"),
+#   make_option("--hkfile", default = "../ref/hkl615.txt", type = "character", 
+#               help = "path to file containing housekeeping genes"),
+#   make_option("--posctrlfile", default = "../ref/oeRegPosCon.txt", 
+#               type = "character",
+#               help = "path to file containing positive control genes"),
+#   make_option("--runQC", default = FALSE, type = "logical",
+#             help = "whether to calculate QC metrics, will error if
+# 	            QC metrics have not previously been calculated"),
+#   make_option("--fast", default = FALSE, type = "logical",
+#             help = "whether to use fast (approximate) PCA"),
+#   make_option("--exclude", default = NULL, type="character", help = "name for excluded samples, if given")
+#   )
