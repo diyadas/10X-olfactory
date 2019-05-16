1f0c38bff6b75627d8ae8acc649c2fdba26bb57f
rkchance <rchance@berkeley.edu>
2019-05-16 16:48:33 -0400

    slurm

--- scripts/tenx_2a_scone_1_eval_slurm.sh
+++ scripts/tenx_2a_scone_1_eval_slurm.sh
@@ -41,7 +41,7 @@ usage() {
 while true; do free -h >> 'memorylogs/tenx_2a_scone_1_eval'$NOW'_memory.out'; sleep 15; done &
 
 run tenx_2a_scone_1_eval.R \
-   env R_LIBS=/share/groups/diya-russell/rpack/3.5/ R --vanilla --args \
+   env R_LIBS=/pylon5/ib5phhp/shared/rpack/3.5/ R --vanilla --args \
        --expt "$expt" --ncores "$ncores" --idfilt "$idfilt" 
 
 
