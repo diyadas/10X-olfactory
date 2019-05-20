92eef0b113ce3f24ce08ab63d1d37634bfa35f3c
rkchance <rchance@berkeley.edu>
2019-05-20 18:57:38 -0400

    whichmerge

---
+++ scripts/tenx_viz_clusterPlots_rsecMerge_slurm.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+#SBATCH --export=ALL
+#SBATCH --nodes=1
+#SBATCH -t 48:00:00
+#SBATCH -p RM
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=diyadas@berkeley.edu,rchance@berkeley.edu
+
+module load gcc
+
+ncores=$1
+method=$2 #zinb or scone
+normalization=$3
+clusmethod=$4 #snn or rsec
+#IF RSEC
+merge=$5 #merged or notmerged
+whichmerge="adjP_cutoff_0.01"
+#whichmerge=$6 #adjP_cutoff_0.01 harcoded for regen/regenk5,  or locfdr_cutoff
+
+#expt="cortex"
+#markerfile="cortexgenes.txt"
+#expt="ob"
+#markerfile="OBmarkers.txt"
+seures="res.0.5"
+expt="regen"
+#expt="regenK5"
+markerfile="oe_markers_regen.txt"
+#markerfile="oe_markers32+regen.txt"
+idfilt="FALSE"
+
+job=$SLURM_JOB_ID
+NOW=$(date +"%Y%m%d-%H%M%S")
+LOG="logs/${expt}_viz_clusterPlots_slurm_rsecMerge_${job}_${NOW}.Rout"
+exec >> "$LOG" 2>&1 || exit 1     # redirect stdout and error to log file, will fail if the logs directory doesn't exist
+
+# as refactored by JW Adams from last commit
+# usage: run input_file command to run
+run() {
+       local input="$1"
+       shift;  # remove file from argument list
+       echo "command: $* < $input"
+       echo "--------------"
+       "$@" < "$input"
+}
+
+usage() {
+       echo "usage: tenx_viz_slurm_clusterPlots.sh ncores method normalization clusmethod" >&2
+       exit 2
+}
+
+[[ $# -eq 5 ]] || usage  # fail if incorrect number of args and print usage info
+
+while true; do free -h >> 'memorylogs/${expt}_tenx_viz_clusterPlots_rsecMerge_slurm_'$NOW'_memory.out'; sleep 15; done &
+
+run tenx_viz_clusterPlots.R \
+  env R_LIBS=/pylon5/ib5phhp/shared/rpack/3.5/ R --vanilla --args \
+--expt "$expt" \
+--ncores "$ncores" \
+--normalization  "$normalization" \
+--method "$method"  \
+--clusmethod "$clusmethod" \
+--markerfile "$markerfile" \
+--seures "$seures" \
+--idfilt "$idfilt" \
+--merge "$merge" \
+--whichmerge "$whichmerge" \
+--samplesort primaryCluster  
