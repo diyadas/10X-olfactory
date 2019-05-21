a537d03263e83ab6df0d63d66a47d491bab0761e
rkchance <rchance@berkeley.edu>
2019-05-21 12:56:02 -0400

    rsec compatable and fxal

--- scripts/tenx_viz_clusterPlots.R
+++ scripts/tenx_viz_clusterPlots.R
@@ -24,7 +24,7 @@ option_list <- list(
   make_option("--samplesort", type = "character", 
               help = "argument clusterSamplesData, i.e. dendrogramValue or primaryCluster"),
   make_option("--idfilt", default = FALSE, type = "logical", help = "logical, has sample ID filtering been performed?"),
-  make_option("--merge", default = FALSE, type = "logical", help = "logical has rsec merging been run, if yes merged"),
+  make_option("--merge", default = "", type = "character", help = "logical has rsec merging been run, if yes merged"),
   make_option("--whichmerge", type= "character", help= "for regen hardcoded for ob or ctx number following locfdr_cutoff_ ")
 )
 
@@ -41,8 +41,15 @@ datdir <- file.path("../output", exptstr, "data")
 vizdir <- file.path("../output", exptstr, "viz")
 ncores <- opt$ncores
 clusmethod <- opt$clusmethod
+merge <- opt$merge
+whichmerge <- opt$whichmerge
 
 register(MulticoreParam(workers = opt$ncores))
+
+
+print(opt)
+mytimestamp <- format(Sys.time(), "%Y%m%d_%H%M%S", tz="America/Los_Angeles")
+print(paste("Files produced by this script will be timestamped:", mytimestamp))
 
 source("tenx_helper.R")
 
@@ -70,7 +77,14 @@ print(paste("Loading this data file: ", datfile))
 load(datfile)
 #load(file.path(datdir, pasteu0(exptstr, "1_se_filtqc", idfiltstr, ".Rda")))
 
-if (opt$clusmethod == "snn"){
+#regenK5_scone_none,fq,qc_k=4,no_bio,batch_snn_idfiltno_20190515_162656.Rda 
+
+if (opt$clusmethod == "snn") {
+datfiles <<- list.files(path = datdir,
+                        pattern = pasteu(exptstr, method, opt$normalization, clusmethod, idfiltstr), full.names = TRUE)
+datfile <- datfiles[length(datfiles)]
+print(paste("Loading this data file: ", datfile))
+load(datfile)
   seu@data <- GetAssayData(object = seu, slot = "counts")
   se_filtered <- se_filtered[, colnames(seu@data)]
   metadata <- seu[, c(grep("^res", colnames(seu))), drop = FALSE]
@@ -81,46 +95,44 @@ if (opt$clusmethod == "snn"){
     message("WARNING: seures not in range of clusterings, using seures as below")
     seures <- colnames(metadata)[1]
     message(paste("seures:", seures))
-  }
-
- if (opt$clusmethod == "rsec") {
-datfiles <<- list.files(path = datdir,
-                        pattern = pasteu(exptstr, method, opt$normalization,
-                                         opt$clusmethod,
-                                         idfiltstr), full.names = TRUE)
-datfile <- datfiles[length(datfiles)]
-print(paste("Loading this data file: ", datfile))
-load(datfile)
-#regen_scone_none,fq,qc_k=4,no_bio,batch_rsec_idfiltno_20190516_090833.Rda
-#regen_scone_none,fq,qc_k=4,no_bio,batch_rsec_plotdendro_adjP_cutoff_0.01_20190517_11422
-} if (opt$merge == "merged") {
-  if (opt$exptstr == "regen" | opt$exptrstr == "regenK5) {
-        datfiles <<- list.files(path = datdir,
-                        pattern = pasteu(exptstr, method, opt$normalization,
-                                         opt$clusmethod, "adjP_cutoff_0.01",
-                                         idfiltstr), full.names = TRUE) 
-   } else if (opt$exptstr == "ob" | opt$exptstr == "cortex" ) {
-         datfiles <<- list.files(path = datdir,
-                        pattern = pasteu(exptstr, method, opt$normalization,
-                                         opt$clusmethod, "locfdr_cutoff_", opt$whichmerge,
-                                         idfiltstr), full.names = TRUE)
-   }
-datfile <- datfiles[length(datfiles)]
-print(paste("Loading this data file: ", datfile))
-load(datfile)
-} 
-
+  } 
   counts <- 2 ^ (seu@data) - 1
   cl <- ClusterExperiment(counts,
                           clusters = as.matrix(metadata),
                           primaryIndex = which(colnames(metadata) == seures),
                           transformation = function(x) log2(x + 1))
-  metadata <- data.frame(expt = colData(se_filtered)$expt, 
-                         batch = colData(se_filtered)$batch,  
+  metadata <- data.frame(expt = colData(se_filtered)$expt,
+                         batch = colData(se_filtered)$batch,
                          samples = colnames(seu@data))
   colData(cl) <- DataFrame(metadata)
 if (opt$method == "zinb"){ reducedDim(cl, "zinbwave") <- seu[["zinbwave@cell.embeddings"]]
-}}
+} }
+
+
+
+#regen_scone_none,fq,qc_k=4,no_bio,batch_rsec_idfiltno_20190516_090833.Rda
+#regen_scone_none,fq,qc_k=4,no_bio,batch_rsec_adjP_mergecutoff_0.01_20190517_114229.Rda
+#regen_scone_none,fq,qc_k=4,no_bio,batch_rsec_plotdendro_adjP_cutoff_0.01_20190517_11422
+if (opt$clusmethod == "rsec") {
+if (opt$merge == "") {
+datfiles <<- list.files(path = datdir,
+                        pattern = pasteu(exptstr, method, opt$normalization,
+                                         opt$clusmethod,
+                                         idfiltstr), full.names = TRUE)
+} else if (opt$merge == "merged") {
+  if (opt$expt == "regen" | opt$expt == "regenK5") {
+        datfiles <<- list.files(path = datdir,
+                        pattern = pasteu(exptstr, method, opt$normalization,
+                                         opt$clusmethod, whichmerge), full.names = TRUE) 
+  } else if (opt$expt == "ob" | opt$expt == "cortex" ) {
+         datfiles <<- list.files(path = datdir,
+                        pattern = pasteu(exptstr, method, opt$normalization,
+                                         opt$clusmethod, whichmerge), full.names = TRUE) }
+datfile <- datfiles[length(datfiles)]
+print(paste("Loading this data file: ", datfile))
+load(datfile)
+cl <- cl2
+} }
 
 
 seed <- 2782472
